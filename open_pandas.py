e890578aaa5340e69cd0448f25c141be1d2e969b
simofleanta <simo.fleanta@gmail.com>
2020-10-08 13:52:42 +0200

    save it

--- open_pandas.py
+++ open_pandas.py
@@ -21,6 +21,29 @@ count_per_company=auto['company'].value_counts()
 print(count_per_company)
 #may be perform charts on count per company or unit 
 
+"""Find the average mileage of each car"""
+average_mileage_per_car=auto['average-mileage'].mean()
+print(average_mileage_per_car) 
+ 
+ 
+"""Find the average mileage of each making company"""
+#grouping by a name or avereage
+
+average_mileage_per_company=auto['company','average-mileage'].mean()
+print(average_mileage_per_company)
+
+"""Sort all cars by Price column """
+#sorting asc or desc
+carsauto=pd.read_csv('a_data.csv')
+carsauto = carsauto.sort_values(by=['price', 'horsepower'], ascending=False)
+carsauto.head(5)
+
+
+
+
+
+
+
 
 
 
