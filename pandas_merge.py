382c0f8d68519c256075bcc1ff8ee885bd858105
simofleanta <simo.fleanta@gmail.com>
2020-11-26 19:50:30 +0100

    s

---
+++ pandas_merge.py
@@ -0,0 +1,7 @@
+#concat - horrizontally 
+conc=pd.concat([year_2020,year_2019])
+#print(conc)
+
+#pd merge good if one df has only 2/3 columns compred t the orther
+merged=pd.merge(year_2019,year_2020)
+#print(merged.columns)
